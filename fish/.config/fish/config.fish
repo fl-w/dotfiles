d399381e7e7618609ffec8f5bbfe8c5885fc0b6e
Fola Bosun <yes@iamfolarin.com>
2020-01-07 21:42:40 +0000

    IEize

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,16 @@
-. ~/.config/fish/aliases.fish
+## Fish config
+#
 
+# Disable fish greeting
 set -gx fish_greeting ''
-set PATH $HOME/.local/.bin $PATH
+
+# Set default editor to nvim
+set -Ux EDITOR /usr/bin/nvim
+
+# Append local bin dir to PATH
+set PATH $HOME/.local/bin $HOME/.bin $PATH
 
 kitty + complete setup fish | source
+
+# Import aliases
+[ -f $HOME/.config/fish/aliases.fish ]; and . ~/.config/fish/aliases.fish
