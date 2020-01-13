c987d881a970f2d3d9f43f4082b93ff21d257f4d
Fola Bosun <yes@iamfolarin.com>
2020-01-13 11:54:12 +0000

    add a dependency check for kitty auto-complete

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,7 +13,7 @@ set -Ux EDITOR /usr/bin/nvim
 # Append local bin dir to PATH
 set PATH $HOME/.local/bin $HOME/.bin $PATH
 
-kitty + complete setup fish | source
+[ -f /usr/bin/kitty ]; and kitty + complete setup fish | source
 
 # Import aliases
 [ -f $HOME/.config/fish/aliases.fish ]; and . ~/.config/fish/aliases.fish
