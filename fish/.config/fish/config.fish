e106fbab287a7de941b9a5e0a793354eee92ce48
Fol O <30976147+folws@users.noreply.github.com>
2020-02-22 13:09:15 +0000

    add n to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,15 +5,27 @@
 set -gx fish_greeting ''
 
 # Set fish colors
-set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
+set -gx LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
 
 # Set default editor to nvim
-set -Ux EDITOR /usr/bin/nvim
+set -gx EDITOR /usr/bin/nvim
+
 
 # Append local bin dir to PATH
 set PATH $HOME/.local/bin $HOME/.bin $PATH
 
+# Setup kitty auto complete
 [ -f /usr/bin/kitty ]; and kitty + complete setup fish | source
+
+# Set FZF to use ag
+[ -f /usr/bin/fzf ]; and [ -f /usr/bin/ag ]; and set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
 
 # Import aliases
 [ -f $HOME/.config/fish/aliases.fish ]; and . ~/.config/fish/aliases.fish
+
+# Set n prefix to home
+set -gx N_PREFIX $HOME/.n
+[ -d $N_PREFIX/bin ]; and set PATH $N_PREFIX/bin $PATH
+
+# Open fish in vim-mode
+fish_vi_key_bindings
