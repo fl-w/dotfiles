ab016ca9cbbe5961af3bbc57f45481ab9ad6f04b
Flo <30976147+folws@users.noreply.github.com>
2020-05-10 03:05:18 +0100

    add flashfocus launch.sh

--- flashfocus/.config/flashfocus/launch.sh
+++ flashfocus/.config/flashfocus/launch.sh
@@ -2,11 +2,5 @@
 
 ## Add this to your wm startup file.
 
-# Terminate already running bar instances
-killall -q flashfocus
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x flashfocus >/dev/null; do sleep 1; done
-
 # Launch flashfocus
 flashfocus &
