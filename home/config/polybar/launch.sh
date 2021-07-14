8eb5fe40b4d96ca95c42305a16b51984da51c0b9
fl-w <30976147+fl-w@users.noreply.github.com>
2021-07-14 12:31:47 +0100

    redo launch script

--- home/config/polybar/launch.sh
+++ home/config/polybar/launch.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env sh
 
 ## Add this to your wm startup file.
-
 bar_type=single
-
-# Terminate already running bar instances
-killall -qe polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ "$THEME" = "light" ]; then COLOR_THEME="theme/light";
 else COLOR_THEME="theme/dark"; fi
-
-export COLOR_THEME
 
 launch_bar() {
     if [ $1 = "split" ]; then
