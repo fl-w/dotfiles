16dfb4e58985b4e90115ed403461ed6626f53e0d
fl-w <30976147+fl-w@users.noreply.github.com>
2021-07-26 12:13:44 +0100

    fix bash profile sourcing

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -1,9 +1,30 @@
 # fish init script
 # @fl-w
 
+# Automatically install fundle (Warning: dangerous on bad connections)
+if not functions -q fundle
+  eval (curl -sfL https://git.io/fundle-install)
+end
+
+fundle plugin 'fl-w/ortega'
+fundle plugin 'franciscolourenco/done'
+fundle plugin 'jethrokuan/z'
+fundle plugin 'oh-my-fish/plugin-await'
+fundle plugin 'oh-my-fish/plugin-license'
+fundle plugin 'edc/bass'
+
+# auto install fundle plugins
+for plugin in (fundle list -s)
+  if not test -d (__fundle_plugins_dir)/$plugin
+    fundle install
+    break
+  end
+end
+
+fundle init
 # source bash profile
 function bsource -a file
-  test -x $file | bass source $file
+  test -x $file && bass source $file
 end
 
 if status --is-login
@@ -54,28 +75,6 @@ _command pipenv; and eval (pipenv --completion)
 
 # Pipr binding
 _command pipr; and bind \ca run-pipr
-
-# Automatically install fundle (Warning: dangerous on bad connections)
-if not functions -q fundle
-  eval (curl -sfL https://git.io/fundle-install)
-end
-
-fundle plugin 'fl-w/ortega'
-fundle plugin 'franciscolourenco/done'
-fundle plugin 'jethrokuan/z'
-fundle plugin 'oh-my-fish/plugin-await'
-fundle plugin 'oh-my-fish/plugin-license'
-fundle plugin 'edc/bass'
-
-# auto install fundle plugins
-for plugin in (fundle list -s)
-  if not test -d (__fundle_plugins_dir)/$plugin
-    fundle install
-    break
-  end
-end
-
-fundle init
 
 # Set prompt options
 set -gx LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
